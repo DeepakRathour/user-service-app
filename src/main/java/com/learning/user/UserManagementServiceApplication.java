e436ff69005247965b45ace48b8d21d76c1abe49
DeepakRathour <http://deepakrathour@github.com/>
2024-07-07 20:04:16 +0530

    Added initial app message

--- src/main/java/com/learning/user/UserManagementServiceApplication.java
+++ src/main/java/com/learning/user/UserManagementServiceApplication.java
@@ -8,6 +8,7 @@ public class UserManagementServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserManagementServiceApplication.class, args);
+		System.out.println("This is a simple rest based user service app");
 	}
 
 }
